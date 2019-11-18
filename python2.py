761f5dbbea4065bc5007d815c9c2279f2874c89c
kelompok002 <57354891+kelompok002@users.noreply.github.com>
2019-11-18 20:32:57 +0700

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,13 @@
+with open( 'Koala.jpg', 'rb') as fd:
+     file_head = fd.read(10)
+     print(file_head)
+  
+with open( 'lamaran kerja.docx', 'rb') as fd:
+     file_head = fd.read(10)
+     print(file_head)
+     
+with open( 'Sleep Away.mp3', 'rb') as fd:
+     file_head = fd.read(10)
+     print(file_head)
+
+
